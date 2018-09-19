377291c048b3f359f36ffcc2ed4ff88169753eb4
Crossy2804 <20002104@tafe.wa.edu.au>
2018-09-19 11:31:42 +0800

    Added manager class and added employees to main

--- EmployeeManagerCSharp/EmployeeManagerCSharp/Program.cs
+++ EmployeeManagerCSharp/EmployeeManagerCSharp/Program.cs
@@ -10,9 +10,19 @@ namespace EmployeeManagerCSharp
     {
         static void Main(string[] args)
         {
-            Employee emp1 = new Employee(123, "Bob");
-            Console.WriteLine(emp1.printDetails());
-
+            Manager emp1 = new Manager(1712, "Ian");
+            //Console.WriteLine(emp1.PrintDetails());
+            Employee emp2 = new Employee(1402, "Tina");
+            Employee emp3 = new Employee(1808, "Julian");
+            Employee emp4 = new Employee(2911, "Emma");
+            Employee emp5 = new Employee(2804, "Sophie");
+            Employee emp6 = new Employee(2006, "Cheech");
+            emp1.AddEmployee(emp2);
+            emp1.AddEmployee(emp3);
+            emp1.AddEmployee(emp4);
+            emp1.AddEmployee(emp5);
+            emp1.AddEmployee(emp6);
+            Console.WriteLine(emp1.PrintDetails());
             Console.ReadLine();
         }
     }
